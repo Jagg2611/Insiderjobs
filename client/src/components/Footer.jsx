b6238bd306080a274f990dff81f12b045dafd357
Jagg2611 <ajmeera_2101mc54@iitp.ac.in>
2025-06-09 10:03:35 +0530

    webname - change

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20">
       <img width={160} src={assets.logo} alt="" />
       <p className="flex-1 border-1 border-gray-100 pl-4 text-sm text-gray-500 max-sm:hidden">
-        Copyright @JobHunt | All rights reserved.
+        Copyright @Insiderjobs | All rights reserved.
       </p>
       <div className="flex gap-2.5">
         <img width={38} src={assets.facebook_icon} alt="" />
